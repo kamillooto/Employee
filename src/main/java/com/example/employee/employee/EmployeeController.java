89484888be247563d77427378284d4c863a3f5a3
Kamil Otocki <kamillooto@gmail.com>
2022-10-15 14:22:50 +0200

    add PUT (updateEmployee)

--- src/main/java/com/example/employee/employee/EmployeeController.java
+++ src/main/java/com/example/employee/employee/EmployeeController.java
@@ -30,4 +30,12 @@ public class EmployeeController {
     public void deleteEmployee(@PathVariable("employeeId") Long employeeId) {
         employeeService.deleteEmployee(employeeId);
     }
+
+    @PutMapping(path = "{employeeId}")
+    public void updateEmployee(
+            @PathVariable("employeeId") Long employeeId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        employeeService.updateEmployee(employeeId, name, email);
+    }
 }
--- src/main/java/com/example/employee/employee/EmployeeService.java
+++ src/main/java/com/example/employee/employee/EmployeeService.java
@@ -3,7 +3,9 @@ package com.example.employee.employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -35,5 +37,23 @@ public class EmployeeService {
             throw new IllegalStateException("Employee with id " + employeeId + " does not exist");
         }
         employeeRepository.deleteById(employeeId);
+    }
+
+    @Transactional
+    public void updateEmployee(Long employeeId, String name, String email) {
+        Employee employee = employeeRepository
+                .findById(employeeId)
+                .orElseThrow(() -> new IllegalStateException("employee with id " + employeeId + " does not exist"));
+        if (name != null && name.length() > 0 && !Objects.equals(name, employee.getName())) {
+            employee.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(email, employee.getEmail())) {
+            Optional<Employee> employeeOptional = employeeRepository.findByEmail(email);
+            if (employeeOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            employee.setEmail(email);
+        }
+
     }
 }
