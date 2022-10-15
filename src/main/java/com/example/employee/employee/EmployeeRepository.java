9e3f52bbc94cb5d62b274375c3ffe0b1b0b22ce3
Kamil Otocki <kamillooto@gmail.com>
2022-10-15 13:42:19 +0200

    add POST (registerNewEmployee)

--- src/main/java/com/example/employee/employee/EmployeeRepository.java
+++ src/main/java/com/example/employee/employee/EmployeeRepository.java
@@ -1,8 +1,14 @@
 package com.example.employee.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("SELECT e FROM Employee e WHERE e.email = ?1")
+    Optional<Employee> findByEmail(String email);
 }
--- src/main/java/com/example/employee/employee/EmployeeService.java
+++ src/main/java/com/example/employee/employee/EmployeeService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -17,5 +18,14 @@ public class EmployeeService {
 
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
+    }
+
+    public void registerNewEmployee(Employee employee) {
+        Optional<Employee> employeeOptional = employeeRepository
+                .findByEmail(employee.getEmail());
+        if (employeeOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        employeeRepository.save(employee);
     }
 }
