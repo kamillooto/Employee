f52b94aa757ee6eeacc74d1776326452bd1218d1
Kamil Otocki <kamillooto@gmail.com>
2022-10-15 13:02:19 +0200

    create EmployeeController, EmployeeService and EmployeeRepository (add GET getEmployee)

---
+++ src/main/java/com/example/employee/employee/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.example.employee.employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
